11c457d92d11bbd27a42d2fb0f750136aaf9cfe7
monkamtanyi <160943000+monkamtanyi@users.noreply.github.com>
2024-11-28 11:43:17 -0500

    Update userdata.sh

--- modules/jenkins-server/userdata.sh
+++ modules/jenkins-server/userdata.sh
@@ -1,3 +1,27 @@
+#WHAT I USED AND IT WORKED TO INSTALL JENKINS
+#Install Java on Ubuntu 24.04 from Apt Package Manager
+
+#!/bin/bash
+# Install Java
+sudo apt update && sudo apt upgrade -y
+sudo apt install default-jdk -y  #install Java 21 on your system.
+java --version
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+
+# Start Jenkins service
+sudo systemctl start jenkins
+sudo systemctl restart jenkins
+sudo systemctl enable jenkins
+
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
 #!/bin/bash
 # Install Java
 sudo apt-get upgrade -y
