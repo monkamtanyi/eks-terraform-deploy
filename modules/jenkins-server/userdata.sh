154ffb22b23fbb515063a748091883fd126785e7
monkamtanyi <160943000+monkamtanyi@users.noreply.github.com>
2024-11-28 16:35:07 -0500

    Update userdata.sh

--- modules/jenkins-server/userdata.sh
+++ modules/jenkins-server/userdata.sh
@@ -19,6 +19,46 @@ sudo apt-get install jenkins
 sudo systemctl start jenkins
 sudo systemctl restart jenkins
 sudo systemctl enable jenkins
+######
+
+#Install Docker using the apt repository
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+#install the latest version, run:
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#Verify that the installation is successful by running the hello-world image:
+sudo docker run hello-world
+#######
+
+#install kubectl for x86-64
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+#Validate the binary (optional)
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+#Confirm that the kubectl binary exists in the current directory:
+ls -l kubectl
+#Verify the Checksum
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+#If the checksum matches, you'll see
+kubectl: OK
+
+XXXXXX
+Change instance type from t2.micro to say t3.medium
+aws ec2 modify-instance-attribute --instance-id i-0b7a90e7a964290a3 --instance-type "t3.medium"
+XXXXXXXX
+
 
 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
